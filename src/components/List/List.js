c8700a8157fc477ef3a72a415c6ed63f86509c06
Lea Mentz <l-mentz@web.de>
2024-01-15 11:59:35 +0100

    add css and delete button without function

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,14 +4,17 @@ export default function List(props) {
   const [filteredWeather, setFilteredWeather] = useState();
   const isGoodWeather = true;
 
-  useEffect(() => {
-    // activities.map(isGoodWeather === true);
-    // setInterval(5);
-  }, []);
+  //   useEffect(() => {
+  //     props.activities.map(isGoodWeather);
+  //     // setInterval(5);
+  //   }, []);
   return (
     <ul>
       {props.activities.map((activity) => (
-        <li>{activity.name}</li>
+        <li>
+          {activity.name}
+          <button>X</button>
+        </li>
       ))}
     </ul>
   );
